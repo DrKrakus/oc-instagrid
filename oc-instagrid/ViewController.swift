a0c5a50afb63ebfac2a38762781ae83709c211cd
DrKrakus <jeromekrakus@macbook-pro-de-jerome.home>
2018-09-14 17:00:18 +0200

    Creation proj

---
+++ oc-instagrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oc-instagrid
+//
+//  Created by Jerome Krakus on 14/09/2018.
+//  Copyright © 2018 Jerome Krakus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
